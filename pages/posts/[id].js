6de7e25ea00263187164020d95c5d39da2e7a9b2
Ruben <34570952+thepixelboy@users.noreply.github.com>
2023-02-18 12:18:44 +0100

    feat: install remark, remark-html. render markdown

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -3,7 +3,7 @@ import { getAllPostIds, getPostData } from "../../lib/posts";
 import Layout from "../../components/layout";
 
 export async function getStaticProps({ params }) {
-  const postData = getPostData(params.id);
+  const postData = await getPostData(params.id);
   return {
     props: {
       postData,
@@ -27,6 +27,8 @@ export default function Post({ postData }) {
       {postData.id}
       <br />
       {postData.date}
+      <br />
+      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
     </Layout>
   );
 }
